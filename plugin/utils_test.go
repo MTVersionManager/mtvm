b5463baad6a193912620bb0904db7c3c6d2cb02b
leomick <83542885+leomick@users.noreply.github.com>
2025-06-12 17:59:07 -0400

    Make plugin test error message first characters lowercase

--- plugin/utils_test.go
+++ plugin/utils_test.go
@@ -13,10 +13,10 @@ import (
 func TestInstalledVersionNoPluginFile(t *testing.T) {
 	_, err := InstalledVersion("loremIpsum", afero.NewMemMapFs())
 	if err == nil {
-		t.Fatal("Want error, got nil")
+		t.Fatal("want error, got nil")
 	}
 	if !errors.Is(err, ErrNotFound) {
-		t.Fatal("Want error to contain ErrNotFound, got error not containing ErrNotFound")
+		t.Fatal("want error to contain ErrNotFound, got error not containing ErrNotFound")
 	}
 }
 
@@ -28,11 +28,11 @@ func TestInstalledVersionEmptyPluginFile(t *testing.T) {
 	}
 	_, err = InstalledVersion("loremIpsum", fs)
 	if err == nil {
-		t.Fatal("Want error, got nil")
+		t.Fatal("want error, got nil")
 	}
 	t.Log(err)
 	if !errors.Is(err, ErrNotFound) {
-		t.Fatal("Want error to contain ErrNotFound, got error not containing ErrNotFound")
+		t.Fatal("want error to contain ErrNotFound, got error not containing ErrNotFound")
 	}
 }
 
@@ -44,25 +44,25 @@ func TestAddFirstEntryNoPluginFile(t *testing.T) {
 		MetadataUrl: "http://example.com",
 	}, fs)
 	if err != nil {
-		t.Fatalf("Want no error, got %v", err)
+		t.Fatalf("want no error, got %v", err)
 	}
 	configDir, err := config.GetConfigDir()
 	if err != nil {
-		t.Fatalf("Want no error when getting config dir, got %v", err)
+		t.Fatalf("want no error when getting config dir, got %v", err)
 	}
 	data, err := afero.ReadFile(fs, filepath.Join(configDir, "plugins.json"))
 	if err != nil {
-		t.Fatalf("Want no error when reading plugins.json, got %v", err)
+		t.Fatalf("want no error when reading plugins.json, got %v", err)
 	}
 	expected := `[
 	{
 		"name": "loremIpsum",
 		"version": "0.0.0",
-		"metadataUrl": "http://example.com"
+		"metadataUrl": "https://example.com"
 	}
 ]`
 	if string(data) != expected {
-		t.Fatalf("Want plugins.json to contain\n%v\ngot plugins.json containing\n%v", expected, string(data))
+		t.Fatalf("want plugins.json to contain\n%v\ngot plugins.json containing\n%v", expected, string(data))
 	}
 }
 
@@ -72,7 +72,7 @@ func TestAddEntryWithExistingEntry(t *testing.T) {
 	{
 		"name": "loremIpsum",
 		"version": "0.0.0",
-		"metadataUrl": "http://example.com"
+		"metadataUrl": "https://example.com"
 	}
 ]`), fs)
 	if err != nil {
@@ -81,53 +81,53 @@ func TestAddEntryWithExistingEntry(t *testing.T) {
 	err = UpdateEntries(Entry{
 		Name:        "dolorSitAmet",
 		Version:     "0.0.0",
-		MetadataUrl: "http://example.com",
+		MetadataUrl: "https://example.com",
 	}, fs)
 	if err != nil {
-		t.Fatalf("Want no error when updating entries, got %v", err)
+		t.Fatalf("want no error when updating entries, got %v", err)
 	}
 	configDir, err := config.GetConfigDir()
 	if err != nil {
-		t.Fatalf("Want no error when getting config directory, got %v", err)
+		t.Fatalf("want no error when getting config directory, got %v", err)
 	}
 	data, err := afero.ReadFile(fs, filepath.Join(configDir, "plugins.json"))
 	if err != nil {
-		t.Fatalf("Want no error when reading plugins.json, got %v", err)
+		t.Fatalf("want no error when reading plugins.json, got %v", err)
 	}
 	expected := `[
 	{
 		"name": "loremIpsum",
 		"version": "0.0.0",
-		"metadataUrl": "http://example.com"
+		"metadataUrl": "https://example.com"
 	},
 	{
 		"name": "dolorSitAmet",
 		"version": "0.0.0",
-		"metadataUrl": "http://example.com"
+		"metadataUrl": "https://example.com"
 	}
 ]`
 	if string(data) != expected {
-		t.Fatalf("Want plugins.json to contain\n%v\ngot plugins.json containing\n%v", expected, string(data))
+		t.Fatalf("want plugins.json to contain\n%v\ngot plugins.json containing\n%v", expected, string(data))
 	}
 }
 
 func CreateAndWritePluginsJson(content []byte, fs afero.Fs) error {
 	configDir, err := config.GetConfigDir()
 	if err != nil {
-		return fmt.Errorf("Want no error when getting config directory, got %v", err)
+		return fmt.Errorf("want no error when getting config directory, got %v", err)
 	}
 	err = fs.MkdirAll(configDir, 0o666)
 	if err != nil {
-		return fmt.Errorf("Want no error when creating config directory, got %v", err)
+		return fmt.Errorf("want no error when creating config directory, got %v", err)
 	}
 	file, err := fs.Create(filepath.Join(configDir, "plugins.json"))
 	if err != nil {
-		return fmt.Errorf("Want no error when creating plugins.json, got %v", err)
+		return fmt.Errorf("want no error when creating plugins.json, got %v", err)
 	}
 	defer file.Close()
 	_, err = file.Write(content)
 	if err != nil {
-		return fmt.Errorf("Want no error when writing to plugins.json, got %v", err)
+		return fmt.Errorf("want no error when writing to plugins.json, got %v", err)
 	}
 	return nil
 }
