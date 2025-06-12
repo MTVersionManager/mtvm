0ee53fc436d0a8129af05015d22ebd852c8f3dac
Leo Mickelson <83542885+leomick@users.noreply.github.com>
2025-06-12 15:36:26 +0000

    WIP Test

--- plugin/utils_test.go
+++ plugin/utils_test.go
@@ -3,13 +3,38 @@ package plugin
 import (
 	"errors"
 	"testing"
+	//"os"
+	//"path/filepath"
+	//"github.com/MTVersionManager/mtvm/config"
 )
 
 func TestNoInstalledVersion(t *testing.T) {
 	_, err := InstalledVersion("loremIpsum")
 	if err == nil {
 		t.Fatal("Want error, got nil")
-	} else if !errors.Is(err, ErrNotFound) {
+	}
+	if !errors.Is(err, ErrNotFound) {
 		t.Fatal("Want error to contain ErrNotFound, got error not containing ErrNotFound")
 	}
 }
+
+// TODO: Finish this test after doing afero
+//func TestAddFirstEntry(t *testing.T) {
+//	err := UpdateEntries(Entry{
+//		Name: "loremIpsum",
+//		Version: "0.0.0",
+//		MetadataUrl: "http://example.com",
+//	})
+//	if err != nil {
+//		t.Fatalf("Want no error, got %v", err)
+//	}
+//	configDir, err := config.GetConfigDir()
+//	if err != nil {
+//		t.Fatalf("Want no error when getting config dir, got %v", err)
+//	}
+//	data, err := os.ReadFile(filepath.Join(configDir, "plugins.json"))
+//	if err != nil {
+//		t.Fatalf("Want no error when reading plugins.json, got %v", err)
+//	}
+//	t.Log(string(data))
+//}
