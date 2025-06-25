37eb9146a1d8712250f11f9f674b1fc28eaeac7a
leomick <83542885+leomick@users.noreply.github.com>
2025-06-25 14:48:42 -0400

    Use testify mock for TestingT in AssertIsNotFoundError

--- shared/testUtils.go
+++ shared/testUtils.go
@@ -1,16 +1,13 @@
 package shared
 
 import (
-	"testing"
-
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
-func AssertIsNotFoundError(t testing.TB, err error, thing string, source Source) {
-	if err == nil {
-		t.Fatal("want error, got nil")
-	}
+func AssertIsNotFoundError(t mock.TestingT, err error, thing string, source Source) {
+	require.NotNil(t, err, "want not nil error, got nil")
 	var notFoundError NotFoundError
 	require.ErrorAs(t, err, &notFoundError)
 	assert.Equalf(t, thing, notFoundError.Thing, "want error to contain thing %v, got %v", thing, notFoundError.Thing)
