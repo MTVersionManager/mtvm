e228e108ed9924ba5e39a498cc629569f4cdd3c8
Leo <83542885+leofleeo@users.noreply.github.com>
2026-01-16 16:50:02 +0000

    first steps of yaegi (WIP, DOES NOT WORK AT ALL!)

--- go.mod
+++ go.mod
@@ -45,6 +45,7 @@ require (
 	github.com/spf13/cast v1.7.1 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/traefik/yaegi v0.16.1 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/crypto v0.37.0 // indirect
--- shared/shared.go
+++ shared/shared.go
@@ -4,7 +4,9 @@ import (
 	"errors"
 	"os"
 	"path/filepath"
+
 	// "strings"
+	"github.com/traefik/yaegi/interp"
 
 	// "github.com/MTVersionManager/goplugin"
 	"github.com/MTVersionManager/mtvmplugin"
@@ -31,11 +33,23 @@ func IsVersionInstalled(tool, version string) (bool, error) {
 }
 
 func LoadPlugin(tool string) (mtvmplugin.Plugin, error) {
+	i := interp.New(interp.Options{})
+	// tool is name, not src, change this!
+	src := ""
+	_, err := i.Eval(src)
+	if err != nil {
+		return nil, err
+	}
+	v, err := i.Eval("plugin.Plugin")
+	if err != nil {
+		return nil, err
+	}
+	plug := v.Interface().(mtvmplugin.Plugin)
 	// var plugin mtvmplugin.Plugin
 	// if strings.ToLower(tool) == "go" {
 	//	plugin = &goplugin.Plugin{}
 	// } else {
-	return nil, errors.New("plugin support is not yet implemented")
+	return plug, errors.New("plugin support is not yet implemented")
 	// }
 	// return plugin, nil
 }
